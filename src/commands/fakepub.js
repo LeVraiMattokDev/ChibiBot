8bbfbfbd2441103181287e400ffca2c3fb2fb21b
Mattok <contact@mattok.ch>
2025-11-13 22:13:27 +0100

    Renommage des commandes

--- src/commands/fakepub.js
+++ src/commands/fakepub.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('spam')
-		.setDescription('Le bot simule l\'envoi de spam.'),
+		.setName('fakepub')
+		.setDescription('Le bot simule l\'envoi d\'une fausse pub.'),
 	async execute(interaction) {
 		await interaction.reply('ATTENTION ! Vous avez gagné un an de poutine gratuite ! Cliquez ici pour réclamer votre prix : <https://www.youtube.com/watch?v=dQw4w9WgXcQ>');
 	},
--- src/commands/fakestats.js
+++ src/commands/fakestats.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('stats')
-		.setDescription('Affiche des statistiques drôles du bot.'),
+		.setName('fakestats')
+		.setDescription('Affiche de fausses statistiques amusantes sur le bot.'),
 	async execute(interaction) {
 		await interaction.reply('**Statistiques Officielles :**\n- Blagues racontées : 7 (dont 2 drôles)\n- Utilisateurs jugés : 42\n- Cafés virtuels consommés : 1337');
 	},
